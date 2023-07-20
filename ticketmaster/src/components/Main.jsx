03d6eaa179d74ce5c7525667212377485e5d3b97
Bryan <b.velez205@gmail.com>
2023-07-19 23:27:18 -0400

    event nav link, event page update, and creation of event details page

--- ticketmaster/src/components/Main.jsx
+++ ticketmaster/src/components/Main.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Venues from "./Venues";
 import Events from "./Events";
 import Cart from "./Cart";
+import EventDetails from "./EventDetails";
 
 const Main = () => {
   return (
     <Routes>
       <Route path="/" element={<Venues />}></Route>
       <Route path="/events" element={<Events />} />
+      <Route path="/events/:id" element={<EventDetails />} />
       <Route path="/cart" element={<Cart />} />
     </Routes>
   );
